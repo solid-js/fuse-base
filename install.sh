938413fc7de2bcab518e31663128241017688a2f
Alexis Bouhet <alexis@cher-ami.tv>
2018-01-15 15:59:28 +0100

    Install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Downloading solid-fuse-base archive..."
-curl -L https://github.com/solid-js/fuse-base/archive/master.zip > fuse-base.zip
+curl -L -sS https://github.com/solid-js/fuse-base/archive/master.zip > fuse-base.zip
 echo "Done !"
 echo ""
 
@@ -9,13 +9,13 @@ echo "Installing archive..."
 unzip -q fuse-base.zip
 rm fuse-base.zip
 mv fuse-base-master/* ./
-mv fuse-base-master/.* ./
+mv fuse-base-master/.gitignore ./
 rm -r fuse-base-master/
 echo "Done !"
 echo ""
 
 echo "Installing node dependencies..."
-npm install
+npm install > npm-install.log
 echo "Done !"
 echo ""
 
